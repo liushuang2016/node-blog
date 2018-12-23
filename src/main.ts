4a081eb2f101bbac20c81815e71dab89a28d79a7
liushuang <liushuangbill@outlook.com>
2018-12-23 22:48:37 +0800

    config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 import { AnyExceptionFilter } from 'src/common/filter/any-exception.filter';
 import { MyLogger } from 'src/common/logger/myLogger';
 import * as morgan from "morgan";
-import { logger } from 'src/common/logger/logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -13,7 +12,7 @@ async function bootstrap() {
   // 异常过滤
   app.useGlobalFilters(new AnyExceptionFilter())
   // 使用管道实现全局参数验证
-  app.useGlobalPipes(new ValidationPipe)
+  app.useGlobalPipes(new ValidationPipe())
   // logger 打印
   app.use(morgan('dev'))
   await app.listen(3000)
