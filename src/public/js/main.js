73013da7f3f1aca5ff61df8eb7373e3460ec1276
liushuang <liushuangbill@outlook.com>
2018-12-28 20:32:51 +0800

    add search

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,12 +1,43 @@
 $(function () {
   var postDir = $('#post-dir')
   var commentsContent = $('#comments-content')
+  var $search = $('#search')
 
   // 设置目录 scroll 事件监听
   setDirPosition(postDir)
   // 设置 回复 功能
   replay(commentsContent)
+  // 搜索功能
+  search($search)
 })
+
+function search($ele) {
+  if (!$ele) {
+    return
+  }
+
+  var $s = $('#input-search')
+  $ele.on('click', function (e) {
+    if ($s.hasClass('hidden')) {
+      $s.removeClass('hidden fadeOut').addClass('fadeIn')
+    } else {
+      $s.removeClass('fadeIn').addClass('fadeOut')
+      setTimeout(function () {
+        $s.addClass('hidden')
+      }, 800)
+    }
+  })
+
+  $s.on('keyup', function (e) {
+    if (e.keyCode === 13) {
+      var val = $s.find('input').val()
+      if (val) {
+        val = val.trim().replace(/\s+/, '+')
+        window.open(`https://www.google.com/search?hl=zh-CN&q=site:liushuang.info+${val}`)
+      }
+    }
+  })
+}
 
 function replay($ele) {
   var width = $(document).outerWidth(true)
