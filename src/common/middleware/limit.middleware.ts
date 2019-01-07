caa681f0a8cb06ff646af04753303f420d6dc330
liushuang <liushuangbill@outlook.com>
2019-01-07 21:17:14 +0800

    fix

--- src/common/middleware/limit.middleware.ts
+++ src/common/middleware/limit.middleware.ts
@@ -4,5 +4,5 @@ import * as rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 1000, // limit each IP to 100 requests per windowMs
+  max: 500, // limit each IP to 100 requests per windowMs
 });
